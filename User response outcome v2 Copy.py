3e04acb7f41bfe01051bc154f18a455c49cc9433
soar_local_admin <noreply@splunk.com>
2024-04-24 10:57:18 +0000

    asd

--- User response outcome v2 Copy.py
+++ User response outcome v2 Copy.py
@@ -413,7 +413,7 @@ def find_related_containers_4(action=None, success=None, container=None, results
     parameters = []
 
     parameters.append({
-        "field_list": None,
+        "field_list": ["userResponse"],
         "value_list": None,
         "minimum_match_count": None,
         "container": id_value,
@@ -443,14 +443,14 @@ def find_related_containers_4(action=None, success=None, container=None, results
 def debug_8(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, loop_state_json=None, **kwargs):
     phantom.debug("debug_8() called")
 
-    find_related_containers_4__result = phantom.collect2(container=container, datapath=["find_related_containers_4:custom_function_result.data"])
+    find_related_containers_4__result = phantom.collect2(container=container, datapath=["find_related_containers_4:custom_function_result.success"])
 
-    find_related_containers_4_data = [item[0] for item in find_related_containers_4__result]
+    find_related_containers_4_success = [item[0] for item in find_related_containers_4__result]
 
     parameters = []
 
     parameters.append({
-        "input_1": find_related_containers_4_data,
+        "input_1": find_related_containers_4_success,
         "input_2": None,
         "input_3": None,
         "input_4": None,
